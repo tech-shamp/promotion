95441a47543c01ca0b10f3e37c086f62ee15642f
tech-shamp <shampytechnical@gmail.com>
2022-08-14 17:44:23 +0500

    ...

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const renderPath = path.join(__dirname, 'public')
+const port = process.env.PORT || 1500
+
+app.set('view engine', 'ejs')
+app.use(express.static(renderPath))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`"""""""""""""""""""`)
+  console.log(`"""""""${port}""""""`)
+  console.log(`"🚀App listening on ${port}"`)
+})
